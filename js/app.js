e70db6dd8aed3f4ef2da415eec644d7a3db69c45
VarozXYZ <alvaropiro@gmail.com>
2026-01-07 18:06:26 +0100

    setup app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let tasks = [];
+
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
